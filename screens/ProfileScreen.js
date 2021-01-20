288cfaf086243d851a7dd38261544c0f37c90138
Georges bitakwire <georges.bitakwire@epitech.eu>
2021-01-20 02:35:15 +0100

    add my info and social info

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,9 +1,20 @@
+const experiences = [
+    {
+       name: 'brynn',
+       description: '',
+       image: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+    },
+   ]
+const 
 import React from 'react';
-import { View,Image, Text, Button, StyleSheet } from 'react-native';
+import { View,Image, Text, SafeAreaView, StyleSheet, Linking } from 'react-native';
+import { Icon, SocialIcon, Card } from 'react-native-elements'
+
 
 const ProfileScreen = ({navigation}) => {
     return (
-        <View style={styles.container}>
+        <SafeAreaView>
+            <View style={styles.container}>
             <View style={styles.header}></View>
             <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>   
             <View style={styles.body}>
@@ -12,6 +23,25 @@ const ProfileScreen = ({navigation}) => {
             </View>
             </View>
         </View>
+        <View style={styles.userInfoSection}>
+            <View style={styles.row}>
+                <Icon name="place" color="#0a0f4c"/>
+                <Text style={{marginLeft: 10}}>Achères, France</Text>
+            </View>
+            <View style={styles.row}>
+                <Icon name="phone" color="#0a0f4c"/>
+                <Text style={{marginLeft: 10}}>06 10 89 68 46</Text>
+            </View>
+            <View style={styles.row}>
+                <Icon name="email" color="#0a0f4c"/>
+                <Text style={{marginLeft: 10}}>georges.bitakwire@epitech.eu</Text>
+            </View>
+            <View style={styles.row}>
+                <SocialIcon type="github" onPress={() => {Linking.openURL('https://github.com/GeorgesBob')}}/>
+                <SocialIcon type="linkedin" onPress= {() => {Linking.openURL('https://www.linkedin.com/in/bitakwire/')}} />
+            </View>
+        </View>
+        </SafeAreaView>
     );
 }
 export default ProfileScreen;
@@ -30,11 +60,11 @@ const styles = StyleSheet.create({
         marginBottom:10,
         alignSelf:'baseline',
         position: 'absolute',
-        marginTop: 130
+        marginTop: 130,
       },
       name:{
         fontSize:22,
-        color:"#0a0f4c",
+        color:"black",
         fontWeight:'600',
       },
       body:{
@@ -44,5 +74,13 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'baseline',
         padding:30,
+      },
+      userInfoSection: {
+          paddingHorizontal: 30,
+          marginBottom: 25,
+      },
+      row: {
+          flexDirection: 'row',
+          marginBottom: 10,
       },
 })
